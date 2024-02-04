0ffd08c884a3aa63436776f63e8fabe97dda24f2
Ben Lupin <benlupin@icloud.com>
2024-02-04 12:35:49 -0400

    Added an example SQL Table, Person and inserted values.

--- MySQLStuff/MySQLTutorial.py
+++ MySQLStuff/MySQLTutorial.py
@@ -10,3 +10,22 @@ db = mysql.connector.connect(
 )
 
 cursor = db.cursor()
+
+# cursor.execute("CREATE DATABASE medicaldb")
+
+## Creating some tables, creating some coloums, and then adding entryies in. 
+## Creating first table
+
+#cursor.execute("CREATE TABLE Person (name VARCHAR(50), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)")
+#cursor.execute("INSERT INTO Person(name, age) VALUES (%s,%s)", ("Josh", 20))
+#db.commit()
+
+cursor.execute("SELECT * FROM Person") #Selects all from the table person and gives it to us
+
+for x in cursor:
+    print(x)
+
+
+
+# Primary Key is the unique number for every single person, 
+# it's generated and different everytime an element is added

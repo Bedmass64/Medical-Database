40edf7f9a80e7b3b59f3e367bdd81fd65caffa2f
SkyJam99 <125305002+SkyJam99@users.noreply.github.com>
2024-02-29 11:41:25 -0400

    Added the pages to the flask app

--- Bedmass-BackEnd/flask_app.py
+++ Bedmass-BackEnd/flask_app.py
@@ -10,3 +10,22 @@ app.config["DEBUG"] = True
 def index():
     return render_template("test_page.html")
 
+@app.route('/admin')
+def admin():
+    return render_template("admin_page.html")
+
+@app.route('/login')
+def login():
+    return render_template("login_page.html")
+
+@app.route('/staff')
+def staff():
+    return render_template("staff_page.html")
+
+@app.route('/createAccount')
+def create_account():
+    return render_template("create_account_page.html")
+
+@app.route('/main')
+def main():
+    return render_template("main_page.html")
